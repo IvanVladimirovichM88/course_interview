b853532e7197e96bac3836b17259551ab136f47d
Ivan_M <opana1988@yandex.ru>
2021-03-28 23:16:01 +0300

    fixed some problem

--- src/main/java/lesson6/MyServlet.java
+++ src/main/java/lesson6/MyServlet.java
@@ -1,15 +1,16 @@
 package lesson6;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
 public class MyServlet extends HttpServlet {
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=utf-8");
